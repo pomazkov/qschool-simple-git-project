f26e698d25d93a1809efafe35635efe0674a26ae
Mikhail Volkov <m.volkov@qsoft.ru>
2021-09-21 17:03:43 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <meta name="author" content="Волков Михаил">
+    
+    <title>Демонстрационный проект</title>
+    
+    <link href="/tailwind.min.css" rel="stylesheet">
+    <style>
+        .word-cell {
+            min-width: 1em;
+        }
+    </style>
+</head>
+<body class="antialiased sans-serif" style="min-width: 375px">
+    <main class="mx-auto max-w-4xl h-screen flex items-center justify-center">
+        <div class="space-y-8">
+            <h1 class="text-4xl text-center">Hello World</h1>
+            <div class="border flex justify-center class bg-white p-4 space-x-2 sm:space-x-4 text-3xl sm:text-4xl md:text-6xl">
+                <div class="word-cell border text-center">Q</div>
+                <div class="word-cell border text-center">S</div>
+                <div class="word-cell border text-center">O</div>
+                <div class="word-cell border text-center">F</div>
+                <div class="word-cell border text-center">T</div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
